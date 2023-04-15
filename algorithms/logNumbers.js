f8eeeee94db90d7647f22a21906fca14dbc49210
James Kelly <jk377y@gmail.com>
2023-04-15 13:27:06 -0500

    logNumbers

--- algorithms/logNumbers.js
+++ algorithms/logNumbers.js
@@ -3,10 +3,10 @@
 //! Feel free to change this value to try for yourself
 let num = 5;
 // Use a for loop to log all numbers from 1 to "num"
-let logNums = function(num) {
-    for (var i = 1; i <= num; i++) {
-    console.log(i);
-    }
+let logNums = function (num) {
+	for (var i = 1; i <= num; i++) {
+		console.log(i);
+	}
 };
 logNums(num);
 
@@ -14,12 +14,12 @@ logNums(num);
 //! Feel free to change this value to try for yourself
 let num2 = 6;
 // this will print the value of i to the console, and then increment i by 1 until i is no longer less than or equal to num2
-let logNums2 = function(num2) {
-    let i = 1;
-    while (i <= num2) {
-    console.log(i);
-    i++;
-    }
+let logNums2 = function (num2) {
+	let i = 1;
+	while (i <= num2) {
+		console.log(i);
+		i++;
+	}
 };
 logNums2(num2);
 
@@ -27,11 +27,11 @@ logNums2(num2);
 //! Feel free to change this value to try for yourself
 let num3 = 7;
 // this will print the value of i to the console, and then decrement i by 1 until i is no longer greater than or equal to 1
-let logNums3 = function(num3) {
-    let i = num3;
-    while (i >= 1) {
-    console.log(i);
-    i--;
-    }
+let logNums3 = function (num3) {
+	let i = num3;
+	while (i >= 1) {
+		console.log(i);
+		i--;
+	}
 };
 logNums3(num3);
